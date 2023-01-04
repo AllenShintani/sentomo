4d27e28529a5c08e3b9d81b067c41c20517ad440
konjikun <shintaniallen@gmail.com>
2023-01-04 23:39:27 +0900

    pc移行とdb再作成完了。サインインの動作確認完了

--- src/pages/rooting/signup.tsx
+++ src/pages/rooting/signup.tsx
@@ -12,33 +12,19 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { useState } from 'react'
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  sendEmailVerification,
-} from 'firebase/auth'
+
 import { FirebaseError } from '@firebase/util'
 import type { FormEvent } from 'react'
 import axios from 'axios'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import exp from 'constants'
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil'
+import Router from 'next/router'
 
 const theme = createTheme()
 // eslint-disable-next-line react-hooks/rules-of-hooks
-const router = useRouter()
-const userMail = atom({
+const router = Router
+const userMail = {
   key: 'userData',
   default: '',
-})
+}
 //usestateでindivdata
 
 //ボタンが押されたときの処理
@@ -95,130 +81,128 @@ export default function SignUp() {
     */
 
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Container
-          component="main"
-          maxWidth="xs"
+    <ThemeProvider theme={theme}>
+      <Container
+        component="main"
+        maxWidth="xs"
+      >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
         >
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography
+            component="h1"
+            variant="h5"
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography
-              component="h1"
-              variant="h5"
-            >
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid
+              container
+              spacing={2}
             >
               <Grid
-                container
-                spacing={2}
+                item
+                xs={12}
+                sm={6}
               >
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                >
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                >
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                >
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                >
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                >
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        value="allowExtraEmails"
-                        color="primary"
-                      />
-                    }
-                    label="I want to receive inspiration, marketing promotions and updates via email."
-                  />
-                </Grid>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
               </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={6}
+              >
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+              >
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+              >
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      value="allowExtraEmails"
+                      color="primary"
+                    />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
 
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid
-                container
-                justifyContent="flex-end"
-              >
-                <Grid item>Already have an account? Sign in</Grid>
-              </Grid>
-            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid
+              container
+              justifyContent="flex-end"
+            >
+              <Grid item>Already have an account? Sign in</Grid>
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
-    </RecoilRoot>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
